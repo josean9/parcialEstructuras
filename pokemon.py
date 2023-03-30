e824af48ed5a5b5943374dd24e99b8e6907323c2
jose antonio <jsancesc@gmail.com>
2023-03-30 11:39:23 +0200

    metodoatacarydefender

--- pokemon.py
+++ pokemon.py
@@ -29,34 +29,46 @@ class pokemon():
                 self.defensa = int(self.defensa)
                 break
             except ValueError:
-                self.defensa = int(input("La defensa debe ser un número, por favor, introduzca un número: "))
+                self.defensa = input("La defensa debe ser un número, por favor, introduzca un número: ")
         while True:
-            if self.salud>10 or self.salud<0:
-                self.salud = int(input("La salud debe ser un número entre 0 y 10, por favor, introduzca un número: "))
-                continue
+            if self.salud>100 or self.salud<0:
+                self.salud = input("La salud debe ser un número entre 0 y 10, por favor, introduzca un número: ")
             else:
                 break
         while True:
             if self.ataque>10 or self.ataque<0:
-                self.ataque = int(input("El ataque debe ser un número entre 0 y 10, por favor, introduzca un número: "))
-                continue
+                self.ataque = input("El ataque debe ser un número entre 0 y 10, por favor, introduzca un número: ")
             else:
                 break
-        """while True:
-            if self.arma == "Puñetazo":
+        while True:
+            if self.defensa > 10 or self.defensa < 0:
+                self.defensa = input("La defensa debe ser un número entre 0 y 10, por favor, introduzca un número: ")
+            else:
                 break
-            elif self.arma == "Patada":
-                break
-            elif self.arma == "Codazo":
-                break
-            elif self.arma == "Cabezazo":
+        while True:
+            if self.arma == "Puñetazo" or self.arma == "Patada" or self.arma == "Codazo" or self.arma == "Cabezazo":
                 break
             else:
                 self.arma = input("El arma debe ser Puñetazo, Patada, Codazo o Cabezazo, por favor, introduzca una de estas opciones: ")
                 print("El arma debe ser Puñetazo, Patada, Codazo o Cabezazo, por favor, introduzca una de estas opciones: ")
-                continue"""
+                continue
+    def is_alive(self):
+        if self.salud>0:
+            return "Esta vivo"
+        else:
+            return "Esta muerto"
+    def fight_defense(self, points_of_damage):
+        if self.defensa>points_of_damage:
+            return ("No se ha podido atacar, la defensa es mayor que el ataque")
+        else:
+            self.salud = self.salud - (points_of_damage - self.defensa)
+            return ("Se ha podido atacar, la nueva salud del pokemon es{}".format(self.salud))
+    def fight_attack(self, pokemon_to_attack):
+        pokemon_to_attack.fight_defense(int(self.ataque))
     def __str__(self):
         return "Pokemon ID: "+str(self.ID)+" con nombre: "+self.nombre+" tiene de arma: "+self.arma+" y salud: "+str(self.salud)
-pokemon1 = pokemon(1, "Pikachu", "Rayo", 30, 50, 30)
+pokemon1 = pokemon(1, "Pikachu", "Puñetazo", 76, 6, 3)
+pokemon2 = pokemon(2, "Charmander", "Patada", 32, 3, 2)
+pokemon3 = pokemon(3, "Bulbasaur", "Codazo", 56, 7, 4)
+pokemon4 = pokemon(4, "Squirtle", "Cabezazo", 34, 5, 6)
 print(pokemon1)
-print("hola")
