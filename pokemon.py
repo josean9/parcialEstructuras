81f585ade4739e68692c7183c3c6626f2ac52283
jose antonio <jsancesc@gmail.com>
2023-03-30 10:53:19 +0200

    commit2

--- pokemon.py
+++ pokemon.py
@@ -31,11 +31,30 @@ class pokemon():
             except ValueError:
                 self.defensa = int(input("La defensa debe ser un número, por favor, introduzca un número: "))
         while True:
-            if arma = "Puñetazo":
-                self.arma = input("El arma debe ser Puñetazo, Patada, Codazo o Cabezazo, por favor, introduzca una de estas opciones: ")
+            if self.salud>10 or self.salud<0:
+                self.salud = int(input("La salud debe ser un número entre 0 y 10, por favor, introduzca un número: "))
                 continue
             else:
                 break
+        while True:
+            if self.ataque>10 or self.ataque<0:
+                self.ataque = int(input("El ataque debe ser un número entre 0 y 10, por favor, introduzca un número: "))
+                continue
+            else:
+                break
+        while True:
+            if self.arma == "Puñetazo":
+                break
+            elif self.arma == "Patada":
+                break
+            elif self.arma == "Codazo":
+                break
+            elif self.arma == "Cabezazo":
+                break
+            else:
+                self.arma = input("El arma debe ser Puñetazo, Patada, Codazo o Cabezazo, por favor, introduzca una de estas opciones: ")
+                print("El arma debe ser Puñetazo, Patada, Codazo o Cabezazo, por favor, introduzca una de estas opciones: ")
+                continue
     def __str__(self):
         return "Pokemon ID: "+str(self.ID)+" con nombre: "+self.nombre+" tiene de arma: "+self.arma+" y salud: "+str(self.salud)
 pokemon1 = pokemon(1, "Pikachu", "Rayo", 30, 50, 30)
